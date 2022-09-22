202d078c130d2b8622e7b3a97da56aaf2cac945a
Billy Dotson <billybad5@gmail.com>
2022-09-22 16:15:44 -0400

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Array!");
+
+int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+string[] friends = new string[5];
+luckyNumbers[1] = 900;
+friends[0] = "Jim";
+friends[1] = "Kelly";
+Console.WriteLine(luckyNumbers[1]);
+Console.WriteLine(friends[0]);
+Console.WriteLine(friends[1]);
+
+Console.ReadLine();
+
+
